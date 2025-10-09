831327bbe22d80c027642419aa6eb20d755d0327
Bmukulu <bmukulu9@gmail.com>
2025-10-09 03:11:42 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+
+// Optional: if you’re using React Router
+// import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Wrap in BrowserRouter if you plan to add pages later */}
+    {/* <BrowserRouter> */}
+      <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
 
